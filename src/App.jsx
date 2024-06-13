191731224738cd69a3ae8c391d6b0e0c56fc5fd6
Hitesh <21amtics465@gmail.com>
2024-06-13 20:19:19 +0530

    final

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import { useState } from 'react'
+import calculator from "./Component/Cal";
+function App() {
+  return (
+    <div>
+      <calculator></calculator>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Cal.jsx
@@ -0,0 +1,13 @@
+function Cal() {
+  return (
+    <>
+      <div className="container">
+        <div className="moneyValues">
+          <label>2000</label>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Cal;
